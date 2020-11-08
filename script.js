f13ace54ba85b9a568377b93e8306e86a1ee78a3
normanmkhl <50177220+normanmkhl@users.noreply.github.com>
2020-11-08 18:31:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,135 @@
+// DOM Elements
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game-container');
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
+
+// List of the words for the game
+
+const words = [
+    'silence',
+    'reflect',
+    'giant',
+    'reporter',
+    'pompous',
+    'grandeur',
+    'bourgeois',
+    'colloquial',
+    'incentive',
+    'zeal',
+    'neat',
+    'physics',
+    'joker',
+    'unforgiven',
+    'scarface',
+    'goodfellas',
+    'sicario',
+    'inception',
+    'wolf',
+    'revenant'
+];
+
+//Init word
+let randomWord;
+
+//Init score
+let score = 0;
+
+//Init time
+let time = 10;
+
+
+//Set difficulty to value in ls or medium
+let difficulty = localStorage.getItem('difficulty') !== null ? 
+localStorage.getItem('difficulty') : 'easy';
+
+//Set difficulty select value
+difficultySelect.value = localStorage.getItem('difficulty') !== null ? 
+localStorage.getItem('difficulty') : 'easy';
+
+//Focus on text on start
+text.focus();
+
+//Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+//Generate random word from array
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+//Add word to DOM
+function addWordToDom() {
+    randomWord = getRandomWord();
+    word.innerHTML = randomWord;
+}
+
+// Update score
+function updateScore() {
+    score++;
+    scoreEl.innerHTML = score;
+}
+
+//Update time
+function updateTime(){
+    time--;
+    timeEl.innerHTML = time + 's';
+
+    if(time === 0) {
+        clearInterval(timeInterval);
+        //end game
+        gameOver(); 
+    }
+}
+
+//Game over end screen
+function gameOver(){
+    endgameEl.innerHTML = `
+        <h1>Time ran out</h1>
+        <p>Your final score is ${score}</p>
+        <button onclick="location.reload()">Reload</button>
+    `;
+
+    endgameEl.style.display = 'flex';
+}
+
+addWordToDom();
+
+//Event listeners
+
+text.addEventListener('input', e => {
+    const insertedText = e.target.value;
+    
+    if(insertedText === randomWord) {
+        addWordToDom();
+        updateScore();
+        
+        //Clear
+        e.target.value = '';
+
+        if(difficulty === 'hard') {
+            time += 2;
+        } else if(difficulty === 'medium') {
+            time += 4
+        } else {
+            time += 6;
+        }
+
+        updateTime();
+    }
+});
+
+//Settings btn click
+settingsBtn.addEventListener('click', () => 
+settings.classList.toggle('hide'));
+
+//Settings select
+settingsForm.addEventListener('change', e => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty',difficulty);
+})
